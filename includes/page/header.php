b1524ce5faa4172dfb1244b6e9a4db2df62baa68
HTF <harderthanfire@gmail.com>
2012-03-10 23:58:18 +0000

    fixed the nav on small screens

--- includes/page/header.php
+++ includes/page/header.php
@@ -38,7 +38,7 @@
       <div class="navbar navbar-fixed-top">
         <div class="navbar-inner">
           <div class="container-fluid">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse" href="./recent">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
