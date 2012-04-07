1d43eaa04bea7329465a753a44b873351429e316
Stephen Swires <stephen.swires@Gmail.com>
2012-04-07 04:11:44 +0100

    - Split up cron jobs to an hourly/daily thing as geshi entries won't need updating as often.

--- cron/daily.php
+++ cron/daily.php
@@ -4,9 +4,6 @@ chdir( "../" ); // change cwdir to pastebin root
 
 include "includes/common.php";
 
-// run database query to delete old pastes
-$db->Query( "DELETE FROM snippets WHERE deleteafter > 0 AND NOW() > DATE_ADD(`time`, INTERVAL `deleteafter` HOUR)");
-
 // check to see if we have any new languages to add to the database
 $languages = $db->QueryArrayIndexed( "SELECT * FROM languages" );
 $dir = opendir( getcwd() . "/includes/geshi/geshi");
---
+++ cron/hourly.php
@@ -0,0 +1,10 @@
+<?php
+
+chdir( "../" ); // change cwdir to pastebin root
+
+include "includes/common.php";
+
+// run database query to delete old pastes
+$db->Query( "DELETE FROM snippets WHERE deleteafter > 0 AND NOW() > DATE_ADD(`time`, INTERVAL `deleteafter` HOUR)");
+
+?>
