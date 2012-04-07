d518dafc3fd6b9dfa5d593434c13f30744b4ac0e
HTF <harderthanfire@gmail.com>
2012-04-08 00:27:57 +0100

    Fixed hourly cron

--- cron/hourly.php
+++ cron/hourly.php
@@ -1,8 +1,9 @@
 <?php
+chdir(dirname(__FILE__)); // change dir to file
 
 chdir( "../" ); // change cwdir to pastebin root
 
-include "includes/common.php";
+include "./includes/common.php";
 
 // run database query to delete old pastes
 $db->Query( "DELETE FROM snippets WHERE deleteafter > 0 AND NOW() > DATE_ADD(`time`, INTERVAL `deleteafter` HOUR)");
