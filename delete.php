c27bfb6f548d5dc18854f4e3e7eb9a03e6c97dc3
HTF <harderthanfire@gmail.com>
2012-04-08 14:45:07 +0100

    Added the missing ;

--- delete.php
+++ delete.php
@@ -10,6 +10,6 @@
 	}
 
 	$did = (int)$_GET["id" ];
-	$db->Query( "DELETE FROM snippets WHERE id='" . $did . "' AND shemail='" .$shemail. "'")
-	header("Location: ./user?delete\r\n");
+	$db->Query( "DELETE FROM snippets WHERE id='" . $did . "' AND shemail='" .$shemail. "'");
+	header("Location: ./user?delete");
 	?>
