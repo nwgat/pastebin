1fe63c4f46111ed6f55e068469cf37c4d4275749
HTF <harderthanfire@gmail.com>
2012-04-19 16:12:21 +0100

    Moved javascript includes to increase page rendering speed

--- includes/page/footer.php
+++ includes/page/footer.php
@@ -5,5 +5,8 @@
         </div><!--/span-->
       </div><!--/row-->
     </div><!--/.fluid-container-->
+      <!-- javascript -->
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
   </body>
   </html>
