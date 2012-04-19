13aa0b50008c2e34a5560f0660eb7d93255d0ba8
HTF <harderthanfire@gmail.com>
2012-04-19 16:20:05 +0100

    Reverted and instead made script calls async

--- includes/page/footer.php
+++ includes/page/footer.php
@@ -5,8 +5,5 @@
         </div><!--/span-->
       </div><!--/row-->
     </div><!--/.fluid-container-->
-      <!-- javascript -->
-  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-  <script src="./js/bootstrap.min.js"></script>
   </body>
   </html>
