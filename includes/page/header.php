45ba021c00a751db5899bdda3b6935553a8223da
HTF <harderthanfire@gmail.com>
2012-04-19 16:23:18 +0100

    REverted as it broke jquery

--- includes/page/header.php
+++ includes/page/header.php
@@ -19,8 +19,8 @@
   <style type="text/css">body{color:#000!important;padding-bottom:40px;padding-top:60px;}#loginbuttons{display:inline-block;margin-right:20px;}#loginbuttons a{width:28px;}#loginbuttons img{vertical-align:middle;}#originalinfo{padding-top:5px;}textarea#code,pre{font-family:Consolas, 'Courier New', courier;}.langr{color:#6C6C6C;font-size:7pt;font-style:italic;}.titler{display:inline-block;width:465px;}.noborder{border:0 none;}.navbar{font-family:Consolas, "Courier New", monospace!important;z-index:0!important;}pre{white-space:pre!important;}.mycode,#alterations{margin-top:10px;}</style>
   
   <!-- javascript -->
-  <script async src="http://code.jquery.com/jquery-latest.min.js"></script>
-  <script async src="./js/bootstrap.min.js"></script>
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
   
   <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
